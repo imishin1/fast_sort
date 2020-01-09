cb34e5e8d540391435072f379b46833c025c1095
imishin1 <gazolin2000@gmail.com>
2020-01-09 13:15:10 +0300

    second commit

--- fast_sort.py
+++ fast_sort.py
@@ -2,9 +2,12 @@ def f_sort(massive):
 	if len(massive) < 2:
 		return massive #базовый случай: массивы с 0 и 1 элементом уже отсортированы
 	else:
-		pivot = massive[0] #рекурсивный случай
-		less = [i for i in massive[1:] if i <= pivot] #подмассив всех элементов, меньших опорного
-		greater = [i for i in massive [1:] if i > pivot] #подмассив всех элементов, больших опорного
+		index_pivot = len(massive)//2
+		pivot = massive[index_pivot] #рекурсивный случай
+		#подмассив всех элементов, меньших опорного
+		less = [i for i in massive[:index_pivot] if i <= pivot] + [i for i in massive[index_pivot + 1:] if i <= pivot]
+		#подмассив всех элементов, больших опорного
+		greater = [i for i in massive [:index_pivot] if i > pivot] + [i for i in massive [index_pivot + 1:] if i > pivot]
 		return f_sort(less) + [pivot] + f_sort(greater)
-a = [1, -124124, -1, 124, 5, 0, 0, 0, 2,14,124,2,1,4,12,4,65,1,25,8,872,36,2,63,236,-124124124124]
+a = [1, -124, -1, 5, 6, 124, 2, 7, 7, 0, -136]
 print(f_sort(a))
